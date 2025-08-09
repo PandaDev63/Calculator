884dfd7fe585b4d109f1f36421901c95dc76c651
PandaDev63 <etiloveridge@gmail.com>
2025-08-09 20:55:21 +1200

    hi

---
+++ index.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    if(display.value == "69"){
+        window.alert("U R SUS");
+    }
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "ERROR"
+    }
+}
