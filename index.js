a8df4ee6bc2650ec668ae8f533d42dbbf2a39c94
PandaDev63 <etiloveridge@gmail.com>
2025-08-13 21:09:22 +1200

    hi

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ function clearDisplay(){
 }
 
 function calculate(){
+    if(display.value == "Infinity69"){
+        window.alert("'SUS, U R.' Yoda said mysteriously...");
+    }
     try{
         display.value = eval(display.value);
     }
@@ -20,7 +23,11 @@ function calculate(){
         display.value = "U R SUS";
     }
     if(display.value == "58008"){
-        let nice = window.prompt("Enter Text:")
+        let nice = window.prompt("Enter Text:");
         display.value = nice;
+    }
+    if(display.value == "jjk"){
+        window.alert("Kaisen Jujutsu Is Better!!!!!!!!!!!!!!");
+        display.value = "KJJ";
     }
 }
