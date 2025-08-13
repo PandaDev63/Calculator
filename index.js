b86fe0943fae24f33f3e2a274bc54d562113ad0f
PandaDev63 <etiloveridge@gmail.com>
2025-08-13 20:56:34 +1200

    nice

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ function clearDisplay(){
 }
 
 function calculate(){
-    if(display.value == "69"){
-        window.alert("U R SUS");
-    }
-    if(display.value == "Infinity69"){
-        window.alert("U TRULY R VERY SUS")
-    }
     try{
         display.value = eval(display.value);
     }
     catch(error){
         display.value = "ERROR"
+    }
+    if(display.value == "69"){
+        window.alert("U R SUS");
+        display.value = "U R SUS";
+    }
+    if(display.value == "58008"){
+        let nice = window.prompt("Enter Text:")
+        display.value = nice;
     }
 }
